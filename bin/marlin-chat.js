96154e4043a6ddb3c50b4828b6e4b4f979501e2a
Stewart Allen <stewart@neuron.com>
2018-02-18 19:17:28 -0500

    checkpoint marlin serial protocol work

--- bin/marlin-chat.js
+++ bin/marlin-chat.js
@@ -1,17 +1,28 @@
 const LineBuffer = require("./LineBuffer");
 const SerialPort = require('serialport');
-
+const opt = require('minimist')(process.argv.slice(2));
 const fs = require('fs');
-const port = process.argv[2];
-const client = new SerialPort(port, { baudRate: 250000 })
+
+const port = opt._[0];                          // serial port name
+const baud = parseInt(opt.baud || "250000");    // baud rate for serial port
+const bufmax = parseInt(opt.buflen || "4");     // max unack'd output lines
+const buf = [];                                 // output buffer
+
+let   waiting = 0;                              // unack'd output lines
+
+const client = new SerialPort(port, { baudRate: baud })
     .on('open', function() {
         console.log("* open: " + port);
     })
-    // .on('data', function(data) {
-    //     console.log("<-- " + data.toString());
-    // })
     .on('line', function(line) {
-        console.log("<-- " + line.toString());
+        if (line == "ok") {
+            if (waiting < bufmax && buf.length) {
+                client.write(buf.shift() + "\n");
+            } else {
+                waiting--;
+            }
+        }
+        console.log("[" + waiting + "] <-- " + line.toString());
     })
     .on('close', function() {
         console.log("* close");
@@ -23,12 +34,25 @@ process.stdin.on("line", line => {
     if (line.indexOf("*send ") === 0) {
         let gcode = fs.readFileSync(line.substring(6)).toString().split("\n");
         gcode.forEach(line => {
-            client.write(line + "\n");
+            send(line);
         });
     } else {
-        client.write(line + "\n");
+        send(line, true);
     }
 });
+
+const send = (line, priority) => {
+    if (waiting < bufmax) {
+        client.write(line + "\n");
+        waiting++;
+    } else {
+        if (priority) {
+            buf.splice(0, 0, line)
+        } else {
+            buf.push(line);
+        }
+    }
+};
 
 new LineBuffer(client);
 new LineBuffer(process.stdin);
