7d48695e3dbf3140a023c2bdb4ab0184bedd9971
Stewart Allen <stewart.allen@gmail.com>
2019-01-10 13:50:28 -0500

    store and display persisted queue

--- web/index.js
+++ web/index.js
@@ -5,29 +5,58 @@ function $(id) {
 function targets(t) {
     let html = [
         '<table><thead><tr>',
-        '<th>target</th>',
-        '<th>info</th>',
-        '<th>filter</th>',
+        cell('th', 'target'),
+        cell('th', 'info'),
+        cell('th', 'filter'),
         '</tr></thead><tbody>'
     ];
     for (let k in t) {
         if (t.hasOwnProperty(k)) {
             let v = t[k];
-            html.push('<tr><th>');
-            html.push(k);
-            html.push('</th><td>');
-            html.push(v.comment || '');
-            html.push('</td><td>');
-            html.push(v.filter || '');
-            html.push('</td></tr>');
+            html.push('<tr>');
+            html.push(cell('th', k));
+            html.push(cell('td', v.comment || ''));
+            html.push(cell('td', v.filter || ''));
+            html.push('</tr>');
         }
     }
     html.push('</tbody></table>');
     $('targets').innerHTML = html.join('');
 }
 
+function cell(type, text) {
+    return ['<', type, '>', text, '</', type, '>'].join('');
+}
+
+function queue(q) {
+    let html = [
+        '<table><thead><tr>',
+        cell('th', 'date'),
+        cell('th', 'to'),
+        cell('th', 'from'),
+        cell('th', 'file'),
+        cell('th', 'status'),
+        '</tr></thead><tbody>'
+    ];
+    q.forEach(el => {
+        let target = el.target.comment || el.target;
+        html.push('<tr>');
+        html.push(cell('td', new Date((el.time || {}).add || 0)));
+        html.push(cell('td', target));
+        html.push(cell('td', el.from));
+        html.push(cell('td', el.name));
+        html.push(cell('td', el.status));
+        html.push('</tr>');
+    });
+    html.push('</tbody></table>');
+    $('queue').innerHTML = html.join('');
+}
+
 function init() {
     fetch("/api/targets")
         .then(r => r.json())
         .then(t => targets(t));
+    fetch("/api/queue")
+        .then(r => r.json())
+        .then(q => queue(q));
 }
