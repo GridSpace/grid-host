b5828a6de06a38e46fe6ec254b6150d59d9030e7
Stewart Allen <stewart.allen@gmail.com>
2019-01-15 13:52:07 -0500

    add for tagging of from ip

--- web/host/index.js
+++ web/host/index.js
@@ -1,8 +1,12 @@
+let lastT = null;
+let lastQ = null;
+
 function $(id) {
     return document.getElementById(id);
 }
 
 function targets(t) {
+    lastT = Object.assign({}, t);
     let html = [
         '<table><thead><tr>',
         cell('th', 'target'),
@@ -24,11 +28,32 @@ function targets(t) {
     $('targets').innerHTML = html.join('');
 }
 
-function cell(type, text) {
+function cell(type, text, opt) {
+    if (opt) {
+        let os = [type];
+        for (let k in opt) {
+            os.push(`${k}="${opt[k]}"`);
+        }
+        type = os.join(' ');
+    }
     return ['<', type, '>', text, '</', type, '>'].join('');
 }
 
+function from_tag(v) {
+    let ktag = `tag-${v}`;
+    let otag = localStorage[ktag]
+    let ntag = prompt(`rename "${v}"`, v);
+    if (ntag === '') {
+        delete localStorage[ktag];
+    } else if (ntag !== null) {
+        localStorage[ktag] = ntag;
+    }
+    console.log({from_tag: v || "ok", ktag, otag, ntag, lastQ});
+    queue(lastQ);
+}
+
 function queue(q) {
+    lastQ = q.slice();
     let html = [
         '<table><thead><tr>',
         cell('th', 'date'),
@@ -41,10 +66,11 @@ function queue(q) {
     ];
     q.reverse().forEach(el => {
         let target = el.target.comment || el.target;
+        let tag = localStorage[`tag-${el.from}`] || el.from;
         html.push('<tr>');
         html.push(cell('td', moment((el.time || {}).add || 0).format('YYYY-MM-DD HH:MM:ss ddd')));
         html.push(cell('td', target));
-        html.push(cell('td', el.from));
+        html.push(cell('td', tag, { onclick:`from_tag('${el.from}')` } ));
         html.push(cell('td', el.name));
         html.push(cell('td', el.size || ''));
         html.push(cell('td', el.status));
