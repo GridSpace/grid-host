4a3ad123deca0a82c24045540da1c3d0052d4a45
Stewart Allen <stewart@neuron.com>
2019-05-03 16:34:13 -0400

    fix image updating. add notification of code update completion

--- bin/update-code.sh
+++ bin/update-code.sh
@@ -5,4 +5,5 @@
 	cd ../grid-bot && git pull && \
 	cd ../grid-host && git pull && \
 	cd ../grid-apps && git pull
+    echo "code update complete"
 ) | tee -a /tmp/update-code.log
--- web/marlin/camera.js
+++ web/marlin/camera.js
@@ -1,10 +1,18 @@
 function init() {
-    setInterval(() => {
-        let time = Date.now();
-        document.documentElement.style.setProperty('--image-url', `url("http://localhost/camera.jpg#${time}")`);
-    }, 1000);
-
     document.getElementById('image').onclick = () => {
         window.location = "http://localhost:4080/index.html";
     };
+
+    updateImage();
+}
+
+function updateImage() {
+    let time = Date.now();
+    let img = new Image();
+    let url = `http://localhost/camera.jpg?time=${time}`;
+    img.onload = () => {
+        document.documentElement.style.setProperty('--image-url', `url(${url})`);
+        updateImage();
+    };
+    img.src = url;
 }
