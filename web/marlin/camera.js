f862fb1329e93ac8f44a7191cbd9f3556fca26ea
Stewart Allen <stewart@neuron.com>
2019-05-03 18:04:27 -0400

    host to hostname

--- web/marlin/camera.js
+++ web/marlin/camera.js
@@ -1,6 +1,6 @@
 function init() {
     document.getElementById('image').onclick = () => {
-        window.location = `http://${location.host}:4080/index.html`;
+        window.location = `http://${location.hostname}:4080/index.html`;
     };
 
     updateImage();
@@ -9,7 +9,7 @@ function init() {
 function updateImage() {
     let time = Date.now();
     let img = new Image();
-    let url = `http://${location.host}:80/camera.jpg?time=${time}`;
+    let url = `http://${location.hostname}/camera.jpg?time=${time}`;
     img.onload = () => {
         document.documentElement.style.setProperty('--image-url', `url(${url})`);
         updateImage();
