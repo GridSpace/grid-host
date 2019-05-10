3ff1ba1c9b617dd2d3bcc61de973edabf8d01263
Stewart Allen <stewart.allen@gmail.com>
2019-05-10 12:48:00 -0400

    use staging tmp file for wifi update

--- bin/update-wifi.sh
+++ bin/update-wifi.sh
@@ -3,10 +3,14 @@
 [ -z "$1" ] && echo "missing ssid" && exit
 [ -z "$2" ] && echo "missing psk" && exit
 
+export TMPF=/tmp/wpa_supplicant.conf
+export FILE=/etc/wpa_supplicant/wpa_supplicant.conf
+
 echo "SSID=${1}"
 echo "PSK=${2}"
+echo "FILE=${FILE}"
 
-sudo cat > /etc/wpa_supplicant/wpa_supplicant.conf << EOF
+cat > ${TMPF} << EOF
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
 country=US
@@ -17,6 +21,7 @@ network={
 }
 EOF
 
-sudo cat /etc/wpa_supplicant/wpa_supplicant.conf
+sudo mv ${TMPF} ${FILE}
+sudo cat ${FILE}
 
 echo "updated wifi settings. reboot required"
