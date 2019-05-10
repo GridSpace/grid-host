1b889d7f9050fb85766efd3fad330a66fff2ff65
Stewart Allen <stewart.allen@gmail.com>
2019-05-10 12:06:23 -0400

    print out updated wifi when done

--- bin/update-wifi.sh
+++ bin/update-wifi.sh
@@ -3,6 +3,9 @@
 [ -z "$1" ] && echo "missing ssid" && exit
 [ -z "$2" ] && echo "missing psk" && exit
 
+echo "SSID=${1}"
+echo "PSK=${2}"
+
 sudo cat > /etc/wpa_supplicant/wpa_supplicant.conf << EOF
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
@@ -13,5 +16,7 @@ network={
     psk="${2}"
 }
 EOF
+
+sudo cat /etc/wpa_supplicant/wpa_supplicant.conf
 
 echo "updated wifi settings. reboot required"
