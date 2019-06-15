edc6d9ac46d12f0dcb81385d5a7e84a0ac74e828
Stewart Allen <stewart@neuron.com>
2019-06-15 12:47:15 -0400

    require sudo on exec. report errors to stdout

--- bin/update-name.sh
+++ bin/update-name.sh
@@ -3,8 +3,8 @@
 [ -z "$1" ] && echo "missing host name" && exit
 
 echo "HOST NAME=${1}"
-sudo echo "${1}" > /etc/hostname
-sudo echo "127.0.0.1 ${1}" >> /etc/hosts
-sudo hostname "${1}"
+echo "${1}" > /etc/hostname || echo "unable to update hostname"
+echo "127.0.0.1 ${1}" >> /etc/hosts || echo "unable to undate hosts"
+hostname "${1}"
 
 echo "updated host name. reboot required"
