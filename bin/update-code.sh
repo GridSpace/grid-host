8173c3c51438af28fdd104525bb52869f5203428
Stewart Allen <stewart.allen@gmail.com>
2020-01-29 21:30:28 -0500

    remove all files that have migrated to the grid-bot package

--- bin/update-code.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-(
-	echo "updating code from github"
-	cd ../grid-bot && git pull && \
-	cd ../grid-host && git pull && \
-	cd ../grid-apps && git pull
-    echo "code update complete"
-) | tee -a /tmp/update-code.log
